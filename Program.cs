af01b4608fa64cd8809b5acadeab6ccdcad6c7ce
serkankorkut17 <srknkrktgs@gmail.com>
2024-05-20 22:43:58 +0300

    ...

--- Program.cs
+++ Program.cs
@@ -103,6 +103,13 @@ internal class Program
 
         app.UseHttpsRedirection();
 
+        app.UseCors(x => x
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials()
+            //.WithOrigins("https://localhost:44351))
+            .SetIsOriginAllowed(origin => true));
+
         app.UseAuthentication();
         app.UseAuthorization();
 

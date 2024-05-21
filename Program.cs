7fb575ea753f2ab729f08a582448e69e90df426e
serkankorkut17 <srknkrktgs@gmail.com>
2024-05-22 00:05:26 +0300

    ..

--- Program.cs
+++ Program.cs
@@ -67,7 +67,7 @@ internal class Program
 
         builder.Services.AddSwaggerGen(option =>
         {
-            option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+            option.SwaggerDoc("v1", new OpenApiInfo { Title = "Leave Master API", Version = "v1" });
             option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
             {
                 In = ParameterLocation.Header,
